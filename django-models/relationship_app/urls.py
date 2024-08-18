78233e40e95e1bd6f1a3eeeec6b3014fb7c447a8
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2024-08-18 22:22:13 +0100

    url hack

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Add URL for library detail page.
     path('login/', LoginView.as_view(), template_name='login'),
     path('logout/', LogoutView.as_view(), template_name='logout'),
+    path('register/', views.register, name='register'),  # Add URL for user registration.
     path('register/', views.UserRegisterView.as_view(), template_name='register'),  # Add URL for user registration.
     path('admin_view/', views.admin_view, name='admin_view'),
     path('librarian_view/', views.librarian_view, name='librarian_view'),
