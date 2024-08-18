db3a8a4537882c990600e325dafbec8e9af067b9
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2024-08-18 17:40:06 +0100

    update url

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -14,7 +14,7 @@ from .models import Book, Library
 # Create your views here.
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
 class LibraryDetailView(DetailView):
