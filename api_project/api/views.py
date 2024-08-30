868d98f0278c898fd390b15e79a6b73618c39f33
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2024-08-30 07:58:26 +0100

    create serializer and corresponding view

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,13 @@
+from rest_framework.generics import ListAPIView
 from django.shortcuts import render
 
+from .models import Book
+from .serializers import BookSerializer
+
 # Create your views here.
+
+
+class BookList(ListAPIView):
+    """Uses serializer to retrieve and return book data"""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
