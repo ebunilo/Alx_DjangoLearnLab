7c27d7bfc886ef8d974ef39c431d5bf7efcaee91
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2024-11-17 22:44:56 +0100

    implement CustomUser and CustomBaseUser

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractUser
 
 
 class Book(models.Model):
@@ -9,3 +10,36 @@ class Book(models.Model):
     def __str__(self):
         return f" Title: {self.title}, Author: {self.author}, Publication Date: {self.publication_year}"
 
+
+# Custom user manager
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, date_of_birth, password):
+        if not email:
+            raise ValueError("Email must be set")
+        if not password:
+            raise ValueError("password is required")
+        
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, date_of_birth=date_of_birth)
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+        
+    def create_superuser(self, username, email, date_of_birth, password):
+        user = self.create_user(username, email, date_of_birth, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.is_active = True
+        user.save(using=self._db)
+        
+        return user
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(blank=True, null=True)
+    profile_photo = models.ImageField(blank=True)
+    
+    objects = CustomUserManager
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
