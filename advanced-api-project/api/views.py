266d7305595ae0e2405b72003920d3ae5ed3f8b3
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2024-11-26 09:26:28 +0100

    implement generic views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Book, Author
+from .serializers import BookSerializer, AuthorSerializer
+
+
+class BookListView(generics.ListAPIView):
+    """List all books"""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+
+class BookDetailView(generics.RetrieveAPIView):
+    """Retrieve a single book"""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookCreateView(generics.CreateAPIView):
+    """Create a new book"""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookUpdateView(generics.UpdateAPIView):
+    """Update an existing book"""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDeleteView(generics.DestroyAPIView):
+    """Delete a book"""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class AuthorListView(generics.ListAPIView):
+    """List all authors"""
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class AuthorCreateView(generics.CreateAPIView):
+    """Create a new author"""
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
