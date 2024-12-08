19b8611e20a184bf6ae8cc4f3d77cd24b748f2ab
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2024-12-08 19:00:45 +0100

    fix typo

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    publishhed_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
