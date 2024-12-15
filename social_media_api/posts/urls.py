8ad3d20a42fd0a93a727b3a98b49a951241176eb
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2024-12-15 23:31:50 +0100

    implement feed

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 
-from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet, FeedView
 from django.urls import path
 from django.urls import include
 
@@ -11,6 +11,7 @@ router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('feed/', FeedView.as_view({'get':'list'})),
     path('/posts/<int:pk>/like/', include('notifications.urls')),
     path('/posts/<int:pk>/unlike/', include('notifications.urls')),
 ]
