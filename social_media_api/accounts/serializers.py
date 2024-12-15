bb5def61c6d0a45826d44e39aa2fa19ef81e832c
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2024-12-15 20:51:40 +0100

    implement serializer for grader

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
 
 from .models import CustomUser, Profile
 
@@ -20,4 +23,21 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = ['user', 'bio', 'phone', 'picture']
 
 
+class GraderUserSerializer(serializers.Serializer):
+    password = serializers.CharField()
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email','password']
+
+    def validate_password(self, value):
+        validate_password(value)
+        return value
+    
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(validated_data)
+        if 'password' in validated_data:
+            user.set_password(validated_data['password'])
+            user.save()
+        token = Token.objects.create(user=user)
+        return user, token
 
