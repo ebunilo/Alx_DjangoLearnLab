794f0c7824e84ba8f6bc0c80759d431ff7f002b7
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2024-12-15 22:45:03 +0100

    Post and Commenaliers

---
+++ social_media_api/posts/urls.py
@@ -0,0 +1,14 @@
+from rest_framework import routers
+
+from .views import PostViewSet, CommentViewSet
+from django.urls import path
+from django.urls import include
+
+router = routers.DefaultRouter()
+
+router.register(r'posts', PostViewSet)
+router.register(r'comments', CommentViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
---
+++ social_media_api/posts/views.py
@@ -0,0 +1,23 @@
+from rest_framework import viewsets, permissions
+from django.shortcuts import render
+
+from .serializers import PostSerializer, CommentSerializer
+from .models import Post, Comment
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
