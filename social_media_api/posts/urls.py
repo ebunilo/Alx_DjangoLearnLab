60baf91b0dd7e32d718dbd044f45b570f89076bb
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2024-12-15 23:28:22 +0100

    add url path for like and unlike

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -10,5 +10,7 @@ router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('/posts/<int:pk>/like/', include('notifications.urls')),
+    path('/posts/<int:pk>/unlike/', include('notifications.urls')),
 ]
